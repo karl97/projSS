062d81b93936d912c686154303167dab55f74f74
karl <karl.97@live.se>
2019-07-25 18:54:10 +0200

    fixed sidescroller
    the world consists of three blocks the one in the back gets destroyed and one new is created in the front when moving forward

---
+++ projSS/Assets/FollowPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    private float xpos;
+    private float startpos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        xpos = 0;
+        startpos = this.transform.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        xpos = player.transform.position.x;
+        this.transform.position = new Vector3(startpos+xpos, this.transform.position.y, this.transform.position.z);
+       
+    }
+}
---
+++ projSS/Assets/Move.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{ 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+    }
+    void OnMouseDrag()
+    {
+        Vector2 mousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        Vector2 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        transform.position = objPosition;
+    }
+}
